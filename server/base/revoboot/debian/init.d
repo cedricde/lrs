cdcc3045bc9bb06331b9e885a49c2f018ca1c485
Linbox FAS <info@linbox.com>
2005-03-31 14:36:26 +0000

    make dep now builds a .deb
    svn path=/trunk/; revision=17

---
+++ server/base/revoboot/debian/init.d
@@ -0,0 +1,43 @@
+#! /bin/sh
+# start/stop the LBS server
+#
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/tftpboot/revoboot/bin/lbs_starter
+NAME=lbs
+DESC="Linbox Backup Server"
+
+test -x $DAEMON || exit 0
+
+set -e
+
+case "$1" in
+  start)
+	echo -n "Starting $DESC: "
+	#start-stop-daemon --start --quiet --background --make-pidfile --pidfile /var/run/$NAME.pid --exec $DAEMON
+	exec $DAEMON
+	;;
+  stop)
+	echo -n "Stopping $DESC: "
+	if [ -x /sbin/start-stop-daemon ]
+	then
+	    start-stop-daemon --stop --quiet --oknodo --pidfile /var/run/$NAME.pid
+	else 
+	    killall -q getClientResponse
+	fi
+	echo "$NAME."
+	;;
+  restart|force-reload)
+	$0 stop
+	$0 start
+	;;
+  *)
+	N=/etc/init.d/$NAME
+	# echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
+	echo "Usage: $N {start|stop|restart|force-reload}" >&2
+	exit 1
+	;;
+esac
+
+exit 0
---
+++ server/base/revoboot/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lbs):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="lbs" command="/usr/bin/lbs"
