821451697944f378987525222ca4a0385e354aa0
Linbox FAS <info@linbox.com>
2007-07-09 15:38:45 +0000

    added batch GNU Prolog
    svn path=/trunk/; revision=305

---
+++ addons/lsc_deployment_scripts/Software_using_a_installer_INNO_SETUP/To_Code/GNU_Prolog/desinstallgnuprolog.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM reg delete renvoi 0 : succés ou 1 : echec
+
+Reg delete "HKLM\SOFTWARE\GNU Prolog" /v Flag /f 
+goto %ERRORLEVEL%
+
+:1
+
+echo Le logiciel GNU Prolog n est pas installe
+exit 1
+GOTO end
+
+
+:0
+
+echo debut de la desinstallation
+cd\
+cd "C:\Program Files\GNU-Prolog"
+"unins000.exe" /sp- /verysilent /norestart
+cd ..
+rmdir "\Program Files\GNU-Prolog" /s /q
+echo Desinstallation terminee
+
+
+:end
